01ee110348dc5c5649bd518ac471cb514c87c41f
Владимир Северов < >
2017-03-22 00:57:30 +0300

    17.03.22 00:17 - Bugs Fix

--- M2-LW-8/M2-LW-8/Source.cpp
+++ M2-LW-8/M2-LW-8/Source.cpp
@@ -1,12 +1,13 @@
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+#include <list>
 
 template<class IT>
 void sort(IT beg, IT end)
 {
-    for (IT i = beg; i < end - 1; ++i) {
-        for (IT j = i + 1; j < end; ++j) {
+    for (IT i = beg; i != end; ++i) {
+        for (IT j = beg; j != end; ++j) {
             if (*j < *i) {
                 std::swap(*i, *j);
             }
@@ -32,6 +33,13 @@ int main() {
     std::vector<double> vec = { 123.0, 3.0, 0.0, 13212.0, -100.0 };
     
     sort(vec.begin(), vec.end()); // vec: -100.0, 0.0, 3.0, 123.0, 13212.0
+    for (size_t i = 0; i < 5; ++i) {
+        std::cout << vec.at(i) << " ";
+    }
+    std::cout << std::endl;
+
+    std::list<double> l = { 123.0, 3.0, 0.0, 13212.0, -100.0 };
+    sort(l.begin(), l.end()); // vec: -100.0, 0.0, 3.0, 123.0, 13212.0
     for (size_t i = 0; i < 5; ++i) {
         std::cout << vec.at(i) << " ";
     }
