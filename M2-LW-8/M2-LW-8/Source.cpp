277ad587f36567eaa48eb1f717b203443259e295
Владимир Северов < >
2017-03-22 00:22:26 +0300

    17.03.22 00:22 - Strange Fix

--- M2-LW-8/M2-LW-8/Source.cpp
+++ M2-LW-8/M2-LW-8/Source.cpp
@@ -16,16 +16,25 @@ void sort(IT beg, IT end)
 
 int main() {
     int arr[] = { 123, 3, 0, 13212, -100 };
+    
     sort(&arr[0], &arr[3]); // arr: 0, 3, 123, 13212, -100
-    for (int& i : arr) std::cout << i << " ";
+    for (size_t i = 0; i < 5; ++i) {
+        std::cout << arr[i] << " ";
+    }
     std::cout << std::endl;
+
     sort(&arr[2], &arr[5]); // arr: 0, 3, -100, 123, 13212
-    for (int& i : arr) std::cout << i << " ";
+    for (size_t i = 0; i < 5; ++i) {
+        std::cout << arr[i] << " ";
+    }
     std::cout << std::endl;
 
     std::vector<double> vec = { 123.0, 3.0, 0.0, 13212.0, -100.0 };
+    
     sort(vec.begin(), vec.end()); // vec: -100.0, 0.0, 3.0, 123.0, 13212.0
-    for (double& i : vec) std::cout << i << " ";
+    for (size_t i = 0; i < 5; ++i) {
+        std::cout << vec.at(i) << " ";
+    }
     std::cout << std::endl;
 
     system("pause");
