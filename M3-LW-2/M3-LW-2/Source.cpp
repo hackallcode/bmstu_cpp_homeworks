1273cc0a49986a63944a1ebd68b44f9f38e277ef
Владимир Северов < >
2017-05-03 14:26:49 +0300

    17.05.03 14:26 - Bug fix

--- M3-LW-2/M3-LW-2/Source.cpp
+++ M3-LW-2/M3-LW-2/Source.cpp
@@ -4,7 +4,7 @@
 template<class It, class Cmp>
 void qsort(It beg, It end, Cmp cmp)
 {
-    It left = beg, right = end;
+    It left = beg, right = end - 1;
     It middle = beg + (end - beg) / 2;
     while (left <= right)
     {
@@ -21,7 +21,7 @@ void qsort(It beg, It end, Cmp cmp)
         }
     }
     if (beg < right) {
-        qsort(beg, right, cmp);
+        qsort(beg, right + 1, cmp);
     }
     if (end > left) {
         qsort(left, end, cmp);
@@ -54,17 +54,17 @@ bool compare(T a, T b) {
 int main() {
     std::vector<std::string> vec{ "5", "3", "4", "1", "2" };
     print(vec);
-    qsort(vec.begin(), --vec.end(), compare<std::string>);
+    qsort(vec.begin(), vec.end(), compare<std::string>);
     print(vec);
 
     std::vector<int> vec2{ 5, 3, 4, 1, 2 };
     print(vec2);
-    qsort(vec2.begin(), --vec2.end(), compare<int>);
+    qsort(vec2.begin(), vec2.end(), compare<int>);
     print(vec2);
 
     std::vector<bool> vec3{ 1, 0, 0, 1, 1, 0, 1 };
     print(vec3);
-    qsort(vec3.begin(), --vec3.end(), compare<bool>);
+    qsort(vec3.begin(), vec3.end(), compare<bool>);
     print(vec3);
 
     system("pause");
